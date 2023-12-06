8919942e3cbcc81bd05f8e4c21f062547d701215
inchanji <iji@ucdavis.edu>
2023-12-06 20:08:01 +0900

    first commit from epyc

--- mount_ftp.sh
+++ mount_ftp.sh
@@ -3,3 +3,4 @@ tgt="/SOC_DATA/건물균열프로젝트/Gkes/지케스 가공데이터"
 #curlftpfs dinsight:Elqdlstkdlxm2!@pcndev.co.kr ./nas_pcn
 #sshfs -o reconnect -p 10200 dinsight@pcndev.co.kr:/ ./nas_pcn #<<< "Elqdlstkdlxm2!"
 sshfs -p 10200 dinsight@pcndev.co.kr:/SOC_DATA/건물균열프로젝트/Gkes/지케스\ 가공데이터 ./nas_pcn #<<< "Elqdlstkdlxm2!"
+
